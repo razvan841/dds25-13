77531b93533cebac86b6bc9411a5a01cf662a4af
Sorin Zele <s.zele@student.tudelft.nl>
2026-02-26 13:16:11 +0100

    small fix

--- order/saga_store.py
+++ order/saga_store.py
@@ -7,7 +7,7 @@ import msgspec
 import redis
 
 from common_kafka.models import Envelope
-from kafka.codec import encode_envelope
+from common_kafka.codec import encode_envelope
 
 # Saga status values
 STATUS_TRYING = "TRYING"
